2051a7438704a5feedefdaf979f56b9e3773c7e3
ssb-kg <105019464+ssb-kg@users.noreply.github.com>
2022-05-14 17:01:57 +0600

    Add files via upload

---
+++ cross(internet shop)/js/app.js
@@ -0,0 +1,205 @@
+
+let tip_crocc_1 = document.querySelector('.tip_cross');
+let tip_crocc_2 = document.querySelector('.tip_cross_2');
+let tip_crocc_3 = document.querySelector('.tip_cross_3');
+let tip_crocc_4 = document.querySelector('.tip_cross_4');
+let tip_crocc_5 = document.querySelector('.tip_cross_5');
+let tip_crocc_6 = document.querySelector('.tip_cross_6');
+let tip_crocc_7 = document.querySelector('.tip_cross_7');
+let tip_crocc_8 = document.querySelector('.tip_cross_8');
+
+
+
+function open_cross(){
+    tip_crocc_1.classList.toggle('tip_cross-active');
+    tip_crocc_2.classList.remove('tip_cross-active');
+    tip_crocc_3.classList.remove('tip_cross-active');
+    tip_crocc_4.classList.remove('tip_cross-active');
+    tip_crocc_5.classList.remove('tip_cross-active');
+    tip_crocc_6.classList.remove('tip_cross-active');
+    tip_crocc_7.classList.remove('tip_cross-active');
+    tip_crocc_8.classList.remove('tip_cross-active');
+}
+
+function open_cross_2(){
+    tip_crocc_1.classList.remove('tip_cross-active');
+    tip_crocc_2.classList.toggle('tip_cross-active');
+    tip_crocc_3.classList.remove('tip_cross-active');
+    tip_crocc_4.classList.remove('tip_cross-active');
+    tip_crocc_5.classList.remove('tip_cross-active');
+    tip_crocc_6.classList.remove('tip_cross-active');
+    tip_crocc_7.classList.remove('tip_cross-active');
+    tip_crocc_8.classList.remove('tip_cross-active');
+}
+
+function open_cross_3(){
+    tip_crocc_1.classList.remove('tip_cross-active');
+    tip_crocc_2.classList.remove('tip_cross-active');
+    tip_crocc_3.classList.toggle('tip_cross-active');
+    tip_crocc_4.classList.remove('tip_cross-active');
+    tip_crocc_5.classList.remove('tip_cross-active');
+    tip_crocc_6.classList.remove('tip_cross-active');
+    tip_crocc_7.classList.remove('tip_cross-active');
+    tip_crocc_8.classList.remove('tip_cross-active');
+}
+
+function open_cross_4(){
+    tip_crocc_1.classList.remove('tip_cross-active');
+    tip_crocc_2.classList.remove('tip_cross-active');
+    tip_crocc_3.classList.remove('tip_cross-active');
+    tip_crocc_4.classList.toggle('tip_cross-active');
+    tip_crocc_5.classList.remove('tip_cross-active');
+    tip_crocc_6.classList.remove('tip_cross-active');
+    tip_crocc_7.classList.remove('tip_cross-active');
+    tip_crocc_8.classList.remove('tip_cross-active');
+}
+
+function open_cross_5(){
+    tip_crocc_1.classList.remove('tip_cross-active');
+    tip_crocc_2.classList.remove('tip_cross-active');
+    tip_crocc_3.classList.remove('tip_cross-active');
+    tip_crocc_4.classList.remove('tip_cross-active');
+    tip_crocc_5.classList.toggle('tip_cross-active');
+    tip_crocc_6.classList.remove('tip_cross-active');
+    tip_crocc_7.classList.remove('tip_cross-active');
+    tip_crocc_8.classList.remove('tip_cross-active');
+}
+
+function open_cross_6(){
+    tip_crocc_1.classList.remove('tip_cross-active');
+    tip_crocc_2.classList.remove('tip_cross-active');
+    tip_crocc_3.classList.remove('tip_cross-active');
+    tip_crocc_4.classList.remove('tip_cross-active');
+    tip_crocc_5.classList.remove('tip_cross-active');
+    tip_crocc_6.classList.toggle('tip_cross-active');
+    tip_crocc_7.classList.remove('tip_cross-active');
+    tip_crocc_8.classList.remove('tip_cross-active');
+}
+
+function open_cross_7(){
+    tip_crocc_1.classList.remove('tip_cross-active');
+    tip_crocc_2.classList.remove('tip_cross-active');
+    tip_crocc_3.classList.remove('tip_cross-active');
+    tip_crocc_4.classList.remove('tip_cross-active');
+    tip_crocc_5.classList.remove('tip_cross-active');
+    tip_crocc_6.classList.remove('tip_cross-active');
+    tip_crocc_7.classList.toggle('tip_cross-active');
+    tip_crocc_8.classList.remove('tip_cross-active');
+}
+
+function open_cross_8(){
+    tip_crocc_1.classList.remove('tip_cross-active');
+    tip_crocc_2.classList.remove('tip_cross-active');
+    tip_crocc_3.classList.remove('tip_cross-active');
+    tip_crocc_4.classList.remove('tip_cross-active');
+    tip_crocc_5.classList.remove('tip_cross-active');
+    tip_crocc_6.classList.remove('tip_cross-active');
+    tip_crocc_7.classList.remove('tip_cross-active');
+    tip_crocc_8.classList.toggle('tip_cross-active');
+}
+
+let color_cross_1 =document.querySelector('.about-cross_color_block_1');
+let color_cross_2 =document.querySelector('.about-cross_color_block_2');
+let color_cross_3 =document.querySelector('.about-cross_color_block_3');
+let color_cross_4 =document.querySelector('.about-cross_color_block_4');
+let color_cross_5 =document.querySelector('.about-cross_color_block_5');
+
+function color_cross_active_1(){
+    color_cross_1.classList.toggle('about-cross_color_block_1-active')
+    color_cross_2.classList.remove('about-cross_color_block_2-active');
+    color_cross_3.classList.remove('about-cross_color_block_3-active');
+    color_cross_4.classList.remove('about-cross_color_block_4-active');
+    color_cross_5.classList.remove('about-cross_color_block_5-active');
+}
+
+function color_cross_active_2(){
+    color_cross_1.classList.remove('about-cross_color_block_1-active');
+    color_cross_2.classList.toggle('about-cross_color_block_2-active');
+    color_cross_3.classList.remove('about-cross_color_block_3-active');
+    color_cross_4.classList.remove('about-cross_color_block_4-active');
+    color_cross_5.classList.remove('about-cross_color_block_5-active');
+}
+
+function color_cross_active_3(){
+    color_cross_1.classList.remove('about-cross_color_block_1-active');
+    color_cross_2.classList.remove('about-cross_color_block_2-active');
+    color_cross_3.classList.toggle('about-cross_color_block_3-active');
+    color_cross_4.classList.remove('about-cross_color_block_4-active');
+    color_cross_5.classList.remove('about-cross_color_block_5-active');
+}
+
+function color_cross_active_4(){
+    color_cross_1.classList.remove('about-cross_color_block_1-active');
+    color_cross_2.classList.remove('about-cross_color_block_2-active');
+    color_cross_3.classList.remove('about-cross_color_block_3-active');
+    color_cross_4.classList.toggle('about-cross_color_block_4-active');
+    color_cross_5.classList.remove('about-cross_color_block_5-active');
+}
+
+function color_cross_active_5(){
+    color_cross_1.classList.remove('about-cross_color_block_1-active');
+    color_cross_2.classList.remove('about-cross_color_block_2-active');
+    color_cross_3.classList.remove('about-cross_color_block_3-active');
+    color_cross_4.classList.remove('about-cross_color_block_4-active');
+    color_cross_5.classList.toggle('about-cross_color_block_5-active');
+}
+
+
+let razmer1 = document.querySelector('.about-cross_razmer_block_1');
+let razmer2 = document.querySelector('.about-cross_razmer_block_2');
+let razmer3 = document.querySelector('.about-cross_razmer_block_3');
+let razmer4 = document.querySelector('.about-cross_razmer_block_4');
+let razmer5 = document.querySelector('.about-cross_razmer_block_5');
+
+function razmer_active_1(){
+    razmer1.classList.toggle('about-cross_razmer_block-acrive')
+    razmer2.classList.remove('about-cross_razmer_block-acrive')
+    razmer3.classList.remove('about-cross_razmer_block-acrive')
+    razmer4.classList.remove('about-cross_razmer_block-acrive')
+    razmer5.classList.remove('about-cross_razmer_block-acrive')
+}
+
+function razmer_active_2(){
+    razmer1.classList.remove('about-cross_razmer_block-acrive')
+    razmer2.classList.toggle('about-cross_razmer_block-acrive')
+    razmer3.classList.remove('about-cross_razmer_block-acrive')
+    razmer4.classList.remove('about-cross_razmer_block-acrive')
+    razmer5.classList.remove('about-cross_razmer_block-acrive')
+}
+
+function razmer_active_3(){
+    razmer1.classList.remove('about-cross_razmer_block-acrive')
+    razmer2.classList.remove('about-cross_razmer_block-acrive')
+    razmer3.classList.toggle('about-cross_razmer_block-acrive')
+    razmer4.classList.remove('about-cross_razmer_block-acrive')
+    razmer5.classList.remove('about-cross_razmer_block-acrive')
+}
+
+function razmer_active_4(){
+    razmer1.classList.remove('about-cross_razmer_block-acrive')
+    razmer2.classList.remove('about-cross_razmer_block-acrive')
+    razmer3.classList.remove('about-cross_razmer_block-acrive')
+    razmer4.classList.toggle('about-cross_razmer_block-acrive')
+    razmer5.classList.remove('about-cross_razmer_block-acrive')
+}
+
+function razmer_active_5(){
+    razmer1.classList.remove('about-cross_razmer_block-acrive')
+    razmer2.classList.remove('about-cross_razmer_block-acrive')
+    razmer3.classList.remove('about-cross_razmer_block-acrive')
+    razmer4.classList.remove('about-cross_razmer_block-acrive')
+    razmer5.classList.toggle('about-cross_razmer_block-acrive')
+}
+
+let menu_buttom = document.querySelector('#menu');
+let side_bar = document.querySelector('.site-bar');
+let list1 = document.querySelector('.list1');
+let list2 = document.querySelector('.list2');
+let list3 = document.querySelector('.list3');
+function opmen_menu(){
+    list1.classList.toggle('list_top');
+    list2.classList.toggle('list_center');
+    list3.classList.toggle('list_right');
+    side_bar.classList.toggle('site-bar_active');
+}
+menu_buttom.addEventListener('click', opmen_menu);
---
+++ cross(internet shop)/js/jordan.js
@@ -0,0 +1,191 @@
+let tip_crocc_1 = document.querySelector('.tip_cross');
+let tip_crocc_2 = document.querySelector('.tip_cross_2');
+let tip_crocc_3 = document.querySelector('.tip_cross_3');
+let tip_crocc_4 = document.querySelector('.tip_cross_4');
+let tip_crocc_5 = document.querySelector('.tip_cross_5');
+let tip_crocc_6 = document.querySelector('.tip_cross_6');
+let tip_crocc_7 = document.querySelector('.tip_cross_7');
+let tip_crocc_8 = document.querySelector('.tip_cross_8');
+
+
+
+function open_cross(){
+    tip_crocc_1.classList.toggle('tip_cross-active');
+    tip_crocc_2.classList.remove('tip_cross-active');
+    tip_crocc_3.classList.remove('tip_cross-active');
+    tip_crocc_4.classList.remove('tip_cross-active');
+    tip_crocc_5.classList.remove('tip_cross-active');
+    tip_crocc_6.classList.remove('tip_cross-active');
+    tip_crocc_7.classList.remove('tip_cross-active');
+    tip_crocc_8.classList.remove('tip_cross-active');
+}
+
+function open_cross_2(){
+    tip_crocc_1.classList.remove('tip_cross-active');
+    tip_crocc_2.classList.toggle('tip_cross-active');
+    tip_crocc_3.classList.remove('tip_cross-active');
+    tip_crocc_4.classList.remove('tip_cross-active');
+    tip_crocc_5.classList.remove('tip_cross-active');
+    tip_crocc_6.classList.remove('tip_cross-active');
+    tip_crocc_7.classList.remove('tip_cross-active');
+    tip_crocc_8.classList.remove('tip_cross-active');
+}
+
+function open_cross_3(){
+    tip_crocc_1.classList.remove('tip_cross-active');
+    tip_crocc_2.classList.remove('tip_cross-active');
+    tip_crocc_3.classList.toggle('tip_cross-active');
+    tip_crocc_4.classList.remove('tip_cross-active');
+    tip_crocc_5.classList.remove('tip_cross-active');
+    tip_crocc_6.classList.remove('tip_cross-active');
+    tip_crocc_7.classList.remove('tip_cross-active');
+    tip_crocc_8.classList.remove('tip_cross-active');
+}
+
+function open_cross_4(){
+    tip_crocc_1.classList.remove('tip_cross-active');
+    tip_crocc_2.classList.remove('tip_cross-active');
+    tip_crocc_3.classList.remove('tip_cross-active');
+    tip_crocc_4.classList.toggle('tip_cross-active');
+    tip_crocc_5.classList.remove('tip_cross-active');
+    tip_crocc_6.classList.remove('tip_cross-active');
+    tip_crocc_7.classList.remove('tip_cross-active');
+    tip_crocc_8.classList.remove('tip_cross-active');
+}
+
+function open_cross_5(){
+    tip_crocc_1.classList.remove('tip_cross-active');
+    tip_crocc_2.classList.remove('tip_cross-active');
+    tip_crocc_3.classList.remove('tip_cross-active');
+    tip_crocc_4.classList.remove('tip_cross-active');
+    tip_crocc_5.classList.toggle('tip_cross-active');
+    tip_crocc_6.classList.remove('tip_cross-active');
+    tip_crocc_7.classList.remove('tip_cross-active');
+    tip_crocc_8.classList.remove('tip_cross-active');
+}
+
+function open_cross_6(){
+    tip_crocc_1.classList.remove('tip_cross-active');
+    tip_crocc_2.classList.remove('tip_cross-active');
+    tip_crocc_3.classList.remove('tip_cross-active');
+    tip_crocc_4.classList.remove('tip_cross-active');
+    tip_crocc_5.classList.remove('tip_cross-active');
+    tip_crocc_6.classList.toggle('tip_cross-active');
+    tip_crocc_7.classList.remove('tip_cross-active');
+    tip_crocc_8.classList.remove('tip_cross-active');
+}
+
+function open_cross_7(){
+    tip_crocc_1.classList.remove('tip_cross-active');
+    tip_crocc_2.classList.remove('tip_cross-active');
+    tip_crocc_3.classList.remove('tip_cross-active');
+    tip_crocc_4.classList.remove('tip_cross-active');
+    tip_crocc_5.classList.remove('tip_cross-active');
+    tip_crocc_6.classList.remove('tip_cross-active');
+    tip_crocc_7.classList.toggle('tip_cross-active');
+    tip_crocc_8.classList.remove('tip_cross-active');
+}
+
+function open_cross_8(){
+    tip_crocc_1.classList.remove('tip_cross-active');
+    tip_crocc_2.classList.remove('tip_cross-active');
+    tip_crocc_3.classList.remove('tip_cross-active');
+    tip_crocc_4.classList.remove('tip_cross-active');
+    tip_crocc_5.classList.remove('tip_cross-active');
+    tip_crocc_6.classList.remove('tip_cross-active');
+    tip_crocc_7.classList.remove('tip_cross-active');
+    tip_crocc_8.classList.toggle('tip_cross-active');
+}
+
+let color_cross_1 =document.querySelector('.about-cross_color_block_1');
+let color_cross_2 =document.querySelector('.about-cross_color_block_2');
+let color_cross_3 =document.querySelector('.about-cross_color_block_3');
+let color_cross_4 =document.querySelector('.about-cross_color_block_4');
+let color_cross_5 =document.querySelector('.about-cross_color_block_5');
+
+function color_cross_active_1(){
+    color_cross_1.classList.toggle('about-cross_color_block_1-active')
+    color_cross_2.classList.remove('about-cross_color_block_2-active');
+    color_cross_3.classList.remove('about-cross_color_block_3-active');
+    color_cross_4.classList.remove('about-cross_color_block_4-active');
+    color_cross_5.classList.remove('about-cross_color_block_5-active');
+}
+
+function color_cross_active_2(){
+    color_cross_1.classList.remove('about-cross_color_block_1-active');
+    color_cross_2.classList.toggle('about-cross_color_block_2-active');
+    color_cross_3.classList.remove('about-cross_color_block_3-active');
+    color_cross_4.classList.remove('about-cross_color_block_4-active');
+    color_cross_5.classList.remove('about-cross_color_block_5-active');
+}
+
+function color_cross_active_3(){
+    color_cross_1.classList.remove('about-cross_color_block_1-active');
+    color_cross_2.classList.remove('about-cross_color_block_2-active');
+    color_cross_3.classList.toggle('about-cross_color_block_3-active');
+    color_cross_4.classList.remove('about-cross_color_block_4-active');
+    color_cross_5.classList.remove('about-cross_color_block_5-active');
+}
+
+function color_cross_active_4(){
+    color_cross_1.classList.remove('about-cross_color_block_1-active');
+    color_cross_2.classList.remove('about-cross_color_block_2-active');
+    color_cross_3.classList.remove('about-cross_color_block_3-active');
+    color_cross_4.classList.toggle('about-cross_color_block_4-active');
+    color_cross_5.classList.remove('about-cross_color_block_5-active');
+}
+
+function color_cross_active_5(){
+    color_cross_1.classList.remove('about-cross_color_block_1-active');
+    color_cross_2.classList.remove('about-cross_color_block_2-active');
+    color_cross_3.classList.remove('about-cross_color_block_3-active');
+    color_cross_4.classList.remove('about-cross_color_block_4-active');
+    color_cross_5.classList.toggle('about-cross_color_block_5-active');
+}
+
+
+let razmer1 = document.querySelector('.about-cross_razmer_block_1');
+let razmer2 = document.querySelector('.about-cross_razmer_block_2');
+let razmer3 = document.querySelector('.about-cross_razmer_block_3');
+let razmer4 = document.querySelector('.about-cross_razmer_block_4');
+let razmer5 = document.querySelector('.about-cross_razmer_block_5');
+
+function razmer_active_1(){
+    razmer1.classList.toggle('about-cross_razmer_block-acrive')
+    razmer2.classList.remove('about-cross_razmer_block-acrive')
+    razmer3.classList.remove('about-cross_razmer_block-acrive')
+    razmer4.classList.remove('about-cross_razmer_block-acrive')
+    razmer5.classList.remove('about-cross_razmer_block-acrive')
+}
+
+function razmer_active_2(){
+    razmer1.classList.remove('about-cross_razmer_block-acrive')
+    razmer2.classList.toggle('about-cross_razmer_block-acrive')
+    razmer3.classList.remove('about-cross_razmer_block-acrive')
+    razmer4.classList.remove('about-cross_razmer_block-acrive')
+    razmer5.classList.remove('about-cross_razmer_block-acrive')
+}
+
+function razmer_active_3(){
+    razmer1.classList.remove('about-cross_razmer_block-acrive')
+    razmer2.classList.remove('about-cross_razmer_block-acrive')
+    razmer3.classList.toggle('about-cross_razmer_block-acrive')
+    razmer4.classList.remove('about-cross_razmer_block-acrive')
+    razmer5.classList.remove('about-cross_razmer_block-acrive')
+}
+
+function razmer_active_4(){
+    razmer1.classList.remove('about-cross_razmer_block-acrive')
+    razmer2.classList.remove('about-cross_razmer_block-acrive')
+    razmer3.classList.remove('about-cross_razmer_block-acrive')
+    razmer4.classList.toggle('about-cross_razmer_block-acrive')
+    razmer5.classList.remove('about-cross_razmer_block-acrive')
+}
+
+function razmer_active_5(){
+    razmer1.classList.remove('about-cross_razmer_block-acrive')
+    razmer2.classList.remove('about-cross_razmer_block-acrive')
+    razmer3.classList.remove('about-cross_razmer_block-acrive')
+    razmer4.classList.remove('about-cross_razmer_block-acrive')
+    razmer5.classList.toggle('about-cross_razmer_block-acrive')
+}
